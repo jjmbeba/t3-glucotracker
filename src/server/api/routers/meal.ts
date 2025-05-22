4facc8d4e645cd1af90e1ec23a7d3f133b44cf5c
Johnson Mbeba <101251043+jjmbeba@users.noreply.github.com>
2025-05-22 18:56:20 +0300

    implement trpc endpoint for deleting meal log

--- src/server/api/routers/meal.ts
+++ src/server/api/routers/meal.ts
@@ -1,8 +1,9 @@
-import { eq } from "drizzle-orm";
+import { and, eq } from "drizzle-orm";
 import { handleTRPCError } from "~/lib/errors";
 import { mealUploadSchema } from "~/schemas/meal";
 import { meal_log } from "~/server/db/schema";
 import { createTRPCRouter, protectedProcedure } from "../trpc";
+import { z } from "zod";
 
 export const mealRouter = createTRPCRouter({
     upload: protectedProcedure.input(mealUploadSchema).mutation(async ({ ctx: { auth, db }, input }) => {
@@ -25,6 +26,25 @@ export const mealRouter = createTRPCRouter({
     getLogs: protectedProcedure.query(async ({ ctx: { db, auth } }) => {
         try {
             return await db.select().from(meal_log).where(eq(meal_log.userId, auth.user.id))
+        } catch (error) {
+            handleTRPCError(error)
+        }
+    }),
+    deleteLog: protectedProcedure.input(z.object({
+        id: z.number()
+    })).mutation(async ({ ctx: { db, auth }, input }) => {
+        try {
+            await db.delete(meal_log).where(
+                and(
+                    eq(meal_log.id, input.id),
+                    eq(meal_log.userId, auth.user.id)
+                )
+            )
+
+            return {
+                success: true,
+                message: "Meal log deleted successfully"
+            }
         } catch (error) {
             handleTRPCError(error)
         }
